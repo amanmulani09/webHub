b68890f6af8480eddc8d93a67790a0c26d1e06c2
amanmulani09 <mulaniaman0504@gmail.com>
2023-03-13 22:26:37 +0530

    added clear videos in history like functionality

--- src/state/globalReducer.js
+++ src/state/globalReducer.js
@@ -4,16 +4,12 @@ const globalReducer = function(state = initialState, action){
     switch(action.type){
         case 'CHANGE_THEME':
             return {...state,isDarkModeOn: action.payload}
-        
         case 'ADD_TO_LIKE':
         return {...state,likedVideos :[...state.likedVideos,action.payload.data] }
-
         case 'ADD_TO_WATCHLIST':
         return {...state,watchLater :[...state.watchLater,action.payload.data] }
-
         case 'ADD_TO_HISTORY':
         return {...state,history :[...state.history,action.payload.data] }
-
         case 'REMOVE_FROM_LIKE':
         const filterdLikedVideos = state.likedVideos.filter((item)=> item.id.videoId !== action.payload.data.id.videoId)
         return {...state,likedVideos:[...filterdLikedVideos]};
